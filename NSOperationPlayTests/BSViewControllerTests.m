4d86dfe166a62fa0b401521a2f4edb1b0135928f
beepscore <support@beepscore.com>
2013-09-14 10:49:29 -0700

    Delete NSOperationPlayTests.m, add BSViewControllerTests.m

---
+++ NSOperationPlayTests/BSViewControllerTests.m
@@ -0,0 +1,34 @@
+//
+//  BSViewControllerTests.m
+//  NSOperationPlay
+//
+//  Created by Steve Baker on 9/14/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BSViewControllerTests : XCTestCase
+
+@end
+
+@implementation BSViewControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
